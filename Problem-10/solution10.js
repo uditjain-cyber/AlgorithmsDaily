ef3c3244c8b74d89e9659d8e6f46c84f8b92224c
Abdullah <42990725+codernuub@users.noreply.github.com>
2019-10-04 00:52:00 +0530

    Implement with javascript

---
+++ Problem-10/solution10.js
@@ -0,0 +1,14 @@
+/*
+.Author : Abdullah khan
+College : Sam Hingginbottom University Of Allahabad
+Year/Department : 1st/BCA
+E-Mail Id : sardarbhai6@gmail.com
+
+Insert Your Code Here*/
+
+
+function reverseWord(str){
+	let w = str.split(" ").reverse();
+	console.log(w);
+}
+reverseWord("I Love Programming.");
